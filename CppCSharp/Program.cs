5ea6bc056074f83069b69e337df2d366942df821
edangolgithub <dangolevan@gmail.com>
2018-06-06 18:28:35 +0545

    Add project files.

---
+++ CppCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EvanDangolCLI;
+namespace CppCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CppBLBridge b = new CppBLBridge();
+            b.add(20, 30);
+        }
+    }
+}
